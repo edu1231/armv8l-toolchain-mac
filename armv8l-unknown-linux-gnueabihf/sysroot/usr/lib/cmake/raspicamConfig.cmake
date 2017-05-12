85293b791a215d9cd20e43b1a63962c32d378b1c
Timo Sandmann <mail@timosandmann.de>
2017-05-12 18:12:34 +0200

    raspicam 0.1.2 added

---
+++ armv8l-unknown-linux-gnueabihf/sysroot/usr/lib/cmake/raspicamConfig.cmake
@@ -0,0 +1,34 @@
+# ===================================================================================
+#  raspicam CMake configuration file
+#
+#             ** File generated automatically, do not modify **
+#
+#  Usage from an external project:
+#    In your CMakeLists.txt, add these lines:
+#
+#    FIND_PACKAGE(raspicam REQUIRED )
+#    TARGET_LINK_LIBRARIES(MY_TARGET_NAME )
+#
+#    This file will define the following variables:
+#      - raspicam_LIBS          : The list of libraries to links against.
+#      - raspicam_LIB_DIR       : The directory where lib files are. Calling LINK_DIRECTORIES
+#                                with this path is NOT needed.
+#      - raspicam_VERSION       : The  version of this PROJECT_NAME build. Example: "1.2.0"
+#      - raspicam_VERSION_MAJOR : Major version part of VERSION. Example: "1"
+#      - raspicam_VERSION_MINOR : Minor version part of VERSION. Example: "2"
+#      - raspicam_VERSION_PATCH : Patch version part of VERSION. Example: "0"
+#
+# ===================================================================================
+INCLUDE_DIRECTORIES(;/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/include)
+LINK_DIRECTORIES("/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/lib")
+
+SET(raspicam_LIBS /usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/lib/vc/libmmal_core.so;/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/lib/vc/libmmal_util.so;/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/lib/vc/libmmal.so raspicam) 
+SET(raspicam_FOUND "YES") 
+
+SET(raspicam_CV_FOUND  "YES")
+SET(raspicam_CV_LIBS /usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/lib/vc/libmmal_core.so;/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/lib/vc/libmmal_util.so;/usr/local/armv8l-unknown-linux-gnueabihf/armv8l-unknown-linux-gnueabihf/sysroot/usr/lib/vc/libmmal.so raspicam opencv_calib3d;opencv_core;opencv_features2d;opencv_flann;opencv_highgui;opencv_imgcodecs;opencv_imgproc;opencv_ml;opencv_objdetect;opencv_photo;opencv_shape;opencv_stitching;opencv_superres;opencv_video;opencv_videoio;opencv_videostab raspicam_cv)
+
+SET(raspicam_VERSION        0.1.2)
+SET(raspicam_VERSION_MAJOR  0)
+SET(raspicam_VERSION_MINOR  1)
+SET(raspicam_VERSION_PATCH  2)
